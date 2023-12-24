d4aae27ee64503e633efb6da76f8ac59c72b2078
arun <you@example.com>
2023-12-24 17:35:56 +0530

    commit

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -10,6 +10,8 @@ interface User {
   realName: string;
 }
 
+const baseURL = "http://localhost:8081";
+
 export default function Chat() {
   const searchParams = useSearchParams();
 
@@ -17,28 +19,26 @@ export default function Chat() {
   const realName = searchParams?.get("realName");
 
   const [connectedUsers, setConnectedUsers] = useState<User[]>([]);
+  const socket = new SockJS(`${baseURL}/ws`);
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<String[]>([]);
+
+  const handleSend = () => {
+    if (message.trim() !== "") {
+      // Add the message to the list of messages
+      setMessages([...messages, message]);
+      // Clear the input field
+      setMessage("");
+    }
+  };
 
   useEffect(() => {
-    // Call connectUser when the component mounts
     connectUser();
-    // findAndDisplayConnectedUsers();
-
-    // Cleanup function (optional)
-    return () => {
-      // Perform cleanup if needed (e.g., disconnecting from the socket)
-    };
-  }, []); // Empty dependency array means this effect runs once when the component mounts
-  const baseUrl = "http://localhost:8081";
+    findAndDisplayConnectedUsers();
+  }, [nickname, realName]);
 
   const connectUser = () => {
-    // Your form submission logic here
-
-    // For example, you can store data in a state, local storage, or make an API call
-
-    // After handling the submission, redirect to the homepage
-    const socket = new SockJS("http://localhost:8081/ws");
     const stompClient = Stomp.over(socket);
-
     stompClient.connect(
       {},
 
@@ -82,16 +82,25 @@ export default function Chat() {
 
   // find and display connected users
   async function findAndDisplayConnectedUsers() {
-    const connectedUsersResponse = await fetch("/users");
-    const connectedUsers: User[] = await connectedUsersResponse.json();
-    const filteredUsers: User[] = connectedUsers.filter(
+    const connectedUsrsResponse = await fetch(`${baseURL}/users`);
+    const connectedUsrs: User[] = await connectedUsrsResponse.json();
+    const filteredUsers: User[] = connectedUsrs.filter(
       (user: User) => user.nickName !== nickname
     );
+    setConnectedUsers(filteredUsers);
+    console.log(filteredUsers);
   }
 
   return (
     <div className="md:m-2">
-      <header className="flex justify-center items-center bg-red-600">
+      <header className="flex justify-between items-center bg-red-600">
+        <p
+          style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
+          className="text-2xl text-white"
+        >
+          {" "}
+          Hi {nickname} {realName}
+        </p>
         <p
           className="py-4 text-base sm:text-lg md:text-xl xl:text-2xl font-bold text-center text-white"
           style={{ textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)" }}
@@ -103,30 +112,44 @@ export default function Chat() {
       <main className="grid grid-cols-4 md:mt-2 md:gap-2 h-[75vh]">
         {/* members section  */}
         <section className="bg-blue-500 ">
-          <h2 className="text-white text-lg font-bold mt-4 mb-2">
+          <h2 className="text-white text-center text-lg font-bold mt-4 mb-2">
             Connected Users
           </h2>
           <ul>
-            {connectedUsers.map((user) => (
-              <li key={user.nickName} className="text-white">
-                {user.realName}
+            {connectedUsers.map((user, index) => (
+              <li key={index} className="text-white text-center font-bold">
+                {user.nickName}
               </li>
             ))}
           </ul>
         </section>
 
         {/* chat section */}
-        <section className="bg-green-500 col-span-3 flex justify-center">
-          {/* message input  */}
-          <div className="flex justify-between items-end w-[70%] mb-8">
+
+        <section className="bg-green-500 col-span-3 flex flex-col justify-center">
+          {/* Display messages */}
+
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className="text-white font-bold  mb-4 mr-56 text-right"
+            >
+              {msg}
+            </div>
+          ))}
+
+          {/* Message input */}
+          <div className="flex items-end mt-auto mx-auto w-[70%] mb-8 ">
             <input
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
               placeholder="Type your message..."
-              // className="w-4/5 px-4 py-2 border rounded-md border-blue-900 bg-stone-200"
               className="w-3/4 px-4 py-2 border rounded-md border-blue-900 bg-gray-200 focus:outline-none focus:ring focus:border-blue-900"
             />
             <button
-              type="submit"
-              className="w-1/5 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-900 border-blue-500"
+              onClick={handleSend}
+              type="button" // Change to "submit" if using a form
+              className="w-1/4 ml-4 px-4 py-2  bg-blue-500 text-white rounded-md hover:bg-blue-900 border-blue-500"
             >
               Send
             </button>
